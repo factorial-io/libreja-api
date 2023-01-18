c0e18263aed92b44c88e62b824bdf3996eca838c
wy118 <wangyang118@gmail.com>
2023-01-18 18:09:10 +0100

    Update UserListLibrary.php

--- src/Resource/User/UserListLibrary.php
+++ src/Resource/User/UserListLibrary.php
@@ -5,7 +5,7 @@ namespace Factorial\Libreja\Resource\User;
 use Factorial\Libreja\Http\HttpRequestGet;
 
 /**
- * Get alle user per library details.
+ * Get all user per library details.
  */
 class UserListLibrary extends HttpRequestGet {
 
