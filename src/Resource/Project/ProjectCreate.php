acccead91eb0caae0a35a04464a9190d48f7781b
Soliman Harkas <soliman@factorial.io>
2023-03-03 12:05:03 +0100

    Use array data in create project

--- src/Resource/Project/ProjectCreate.php
+++ src/Resource/Project/ProjectCreate.php
@@ -12,16 +12,8 @@ class ProjectCreate extends HttpRequestPut {
   /**
    * {@inheritdoc}
    */
-  public function __construct($libId, $customer, $name, $externalProjectNr, $startDate, $endDate, $format="") {
-    parent::__construct('/project/new', [
-      'library' => $libId,
-      'customer' => $customer,
-      'name' => $name,
-      'nr' => $externalProjectNr,
-      'begin' => $startDate,
-      'end' => $endDate,
-      'format' => $format
-    ]);
+  public function __construct($data) {
+    parent::__construct('/project/new', $data);
   }
 
 }
